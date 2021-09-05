7cb47dd969a8c341db8c48b086b95583d081cf0e
yousefgadban <yousef3455@gmail.com>
2021-09-05 11:46:27 +0300

    add anim

---
+++ index.js
@@ -0,0 +1,32 @@
+// const element = document.getElementById("intro");
+
+// const title = document.getElementsByClassName("title");
+
+// title.style.color = 'red'
+
+
+
+
+// const body = document.getElementById("body");
+
+// var body = element.scrollTop;
+// console.log(body);
+
+// document.getScroll = function() {
+//     if (window.pageYOffset != undefined) {
+//         return [pageXOffset, pageYOffset];
+//     } else {
+//         var sx, sy, d = document,
+//             r = d.documentElement,
+//             b = d.body;
+//         sx = r.scrollLeft || b.scrollLeft || 0;
+//         sy = r.scrollTop || b.scrollTop || 0;
+//         return [sx, sy];
+//     }
+// }
+
+
+// window.addEventListener("scroll", (event) => {
+//     let scroll = this.scrollY;
+//     console.log(scroll)
+// });
